0373518489fdfc1aa90e641e8752f6ce15ed6bc0
DJ Rajkhowa <deepjyot@netapp.com>
2021-01-12 13:42:56 -0500

    Fix prettier formatting issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,31 +4,31 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [RouterTestingModule],
-			declarations: [AppComponent],
-		}).compileComponents();
-	});
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
 
-	it('should create the app', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app).toBeTruthy();
-	});
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-	it(`should have as title 'bcg-dashboard'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.title).toEqual('bcg-dashboard');
-	});
+  it(`should have as title 'bcg-dashboard'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('bcg-dashboard');
+  });
 
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiledEl: DebugElement = fixture.debugElement;
-		const compiledNativeEl = compiledEl.nativeElement;
-		const span = compiledNativeEl.querySelector('.content span');
-		expect(span.textContent).toContain('bcg-dashboard app is running!');
-	});
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiledEl: DebugElement = fixture.debugElement;
+    const compiledNativeEl = compiledEl.nativeElement;
+    const span = compiledNativeEl.querySelector('.content span');
+    expect(span.textContent).toContain('bcg-dashboard app is running!');
+  });
 });
